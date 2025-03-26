dd400bb9539e7a608a7e3d6404084bce46b21ac9
sysnote8main <sysnote8main@gmail.com>
2025-03-26 14:12:32 +0900

    feat: initial commit for preparing migration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { NextIntlClientProvider } from 'next-intl';
+import { useRouter } from 'next/router';
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <NextIntlClientProvider
+      locale={router.locale}
+      timeZone='Asia/Tokyo'
+      messages={pageProps.messages}
+    >
+      <Component {...pageProps} />
+    </NextIntlClientProvider>
+  );
 }
